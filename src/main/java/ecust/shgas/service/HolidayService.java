dd0a2d9e8729998800357568bf25b2e183f4b14e
huchao <1083763917@qq.com>
2017-06-22 21:08:47 +0800

    线程池实现多任务初期2

---
+++ src/main/java/ecust/shgas/service/HolidayService.java
@@ -0,0 +1,14 @@
+package ecust.shgas.service;
+
+import java.util.List;
+
+/**
+ * Created by huchao on 2017/6/21.
+ */
+ public interface HolidayService {
+    /*
+           获取节假日单天时间段话务统计
+     */
+   List  getPeriodHotxx(String condition);
+}
+

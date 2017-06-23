01e0ce0c090b275535bd3eda633c07df8da9bf58
huchao <1083763917@qq.com>
2017-06-23 13:43:32 +0800

    Jdbc sql语句拼接封装

--- src/main/java/ecust/shgas/dao/hive/HolidayDao.java
+++ src/main/java/ecust/shgas/dao/hive/HolidayDao.java
@@ -1,10 +1,11 @@
 package ecust.shgas.dao.hive;
+
 import ecust.shgas.task.HiveTask;
+import ecust.shgas.util.SqlTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by huchao on 2017/6/21.
@@ -13,17 +14,25 @@ import java.util.List;
 public class HolidayDao {
     @Autowired
     private HiveTask hiveTask;
+    @Autowired
+    private SqlTool sqlTool;
 
     public List getHotxx(String condition) {
+        Map<String,List> maps = new LinkedHashMap<String, List>();
+        List limits = new ArrayList();
         String table_name = "hotxx_" + condition;
         String begin_time = condition + "-10-01";
         String end_time = condition + "-10-07";
-        String sql1 = "SELECT day(create_time) as day,count(*) as huawu_count FROM " + table_name + " where create_time BETWEEN " + "'" + begin_time + "'" + " and " + "'" + end_time + "'" + " group by day(create_time) ORDER BY day asc";
-        String sql2 = "SELECT hour(create_time) as hour,count(*) as huawu_count FROM " + table_name + " where create_time BETWEEN " + "'" + begin_time + "'" + " and " + "'" + end_time + "'" + " group by hour(create_time) ORDER BY hour asc";
-        List<String> sqls = new ArrayList<String>();
-        sqls.add(sql1);
-        sqls.add(sql2);
-        hiveTask.setSqls(sqls);
+        String sql1 = "SELECT day(create_time) as day,count(*) as huawu_count FROM ? where create_time BETWEEN ?  and ? group by day(create_time) ORDER BY day asc";
+        String sql2 = "SELECT hour(create_time) as hour,count(*) as huawu_count FROM ? where create_time BETWEEN ?  and ? group by hour(create_time) ORDER BY hour asc";
+        limits.add(table_name);
+        limits.add(begin_time);
+        limits.add(end_time);
+        maps.put(sql1, limits);
+        maps.put(sql2, limits);
+        sqlTool.setMap(maps);
+        List<String> res= sqlTool.sqlset();
+        hiveTask.setSqls(res);
         List results = hiveTask.execute();
         return results;
     }
---
+++ src/main/java/ecust/shgas/util/SqlTool.java
@@ -0,0 +1,52 @@
+package ecust.shgas.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by huchao on 2017/6/23.
+ */
+@Component
+public class SqlTool {
+    private Map<String, List> map;
+    public Map getMap() {
+        return map;
+    }
+    public void setMap(Map map) {
+        this.map = map;
+    }
+    public List<String> sqlset(){
+        String split_char = "\\?";//sql字符串分隔符
+        String table_prefix = "hotxx_";//表前缀
+        List<String> sqlCollection = new LinkedList<String>();
+        for (String key : this.map.keySet()) {
+            StringBuilder sqlConnect = new StringBuilder();
+            List limits = this.map.get(key);
+            String[] sqls = key.split(split_char);
+            //查询条件数
+            int con_len = limits.size();
+            //以问号进行分割sql语句,进行sql字符串拼接
+            int sql_len = sqls.length;
+            for (int i = 0; i < con_len; i++) {
+                if(limits.get(i) instanceof String){
+                    boolean isMatch = ((String) limits.get(i)).contains(table_prefix);
+                        if(isMatch){
+                            sqlConnect.append(sqls[i]+limits.get(i));
+                        }else{
+                            sqlConnect.append(sqls[i] + "'"+limits.get(i)+"'");
+                        }
+                }else{
+                    sqlConnect.append(sqls[i]+limits.get(i));
+                }
+            }
+            if (sql_len == con_len + 1) {
+                sqlConnect.append(sqls[con_len]);
+            }
+            sqlCollection.add(sqlConnect.toString());
+        }
+        return sqlCollection;
+    }
+}
